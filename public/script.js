6c3af01cd2336414955de90743ba2e8b33a062ca
10student <10student@nr.stu>
2019-04-02 12:45:36 -0400

    added js file for login

---
+++ public/script.js
@@ -0,0 +1,18 @@
+// JavaScript source code
+// WHEN THE DOCUMENT IS READYYYY, DO THISSSSS
+// it means the DOM has completed loading all of the nodes for this page
+$(document).ready(function () {
+    // Handler for .ready() called.
+    $("#invalidlogin").hide();
+    $("#usernamewarning").hide();
+    $("#passwarning").hide();
+    $("#login").removeClass("submitinactive")
+
+    $("#login").click(function (e) {
+        e.preventDefault();
+        $("#invalidlogin").show();
+    });
+    $("#username").keypress(function() {
+        console.log 
+})
+});
